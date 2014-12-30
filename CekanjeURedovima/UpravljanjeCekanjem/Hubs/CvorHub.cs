13d83266ffa2201c2b9c035b349fbe8f85870da7
ANovokmet <ante.novokmet@fer.hr>
2014-12-30 20:42:19 +0100

    sprema veličinu sata u bazu

--- CekanjeURedovima/UpravljanjeCekanjem/Hubs/CvorHub.cs
+++ CekanjeURedovima/UpravljanjeCekanjem/Hubs/CvorHub.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using Microsoft.AspNet.SignalR;
-
+using UpravljanjeCekanjem.Models;
 namespace UpravljanjeCekanjem
 {
     public class CvorHub : Hub
@@ -12,6 +12,19 @@ namespace UpravljanjeCekanjem
         {
             System.Diagnostics.Debug.WriteLine("" + poruka);
             Clients.All.pokreni(poruka+"px");
+            Osvjezi_postavku(0, Convert.ToInt32(poruka));
+        }
+
+        public void Osvjezi_postavku(int id, int value)
+        {
+            using (var db = new DataBaseEntities())
+            {
+                Postavke postavka = new Postavke();
+                postavka = db.Postavke.Where(s => s.Identifikator == id).FirstOrDefault<Postavke>();// ili db.Postavke.FirstOrDefault(c => c.Identifikator == id); 
+                postavka.vrijednost = value;
+                db.Entry(postavka).State = System.Data.Entity.EntityState.Modified;
+                db.SaveChanges();
+            }
         }
     }
 }
