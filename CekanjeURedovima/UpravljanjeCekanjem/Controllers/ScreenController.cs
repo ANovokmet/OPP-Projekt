58aef15c3e0c5883f0a5a9868b3d9a7c10e80066
ANovokmet <ante.novokmet@fer.hr>
2015-01-03 20:02:55 +0100

    dodan statični screen
    kasnije to sredim signalRom

--- CekanjeURedovima/UpravljanjeCekanjem/Controllers/ScreenController.cs
+++ CekanjeURedovima/UpravljanjeCekanjem/Controllers/ScreenController.cs
@@ -3,18 +3,53 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using UpravljanjeCekanjem.Models;
 
 namespace UpravljanjeCekanjem.Controllers
 {
+    [AllowAnonymous]
     public class ScreenController : Controller
     {
         //
         // GET: /Screen/
+        DataBaseEntities db = new DataBaseEntities();
 
-        public ActionResult Index()
+        public ActionResult Index()//u viewu treba hrpa jquerija zbog signalR i realtime, ovo je za probu, prebacit u cvorhub
         {
             ViewBag.broj = "BROJ KOJI FLASHA";
-            return View();
+
+            Dictionary<string, int> redovi = new Dictionary<string,int>();
+
+            using(var db = new DataBaseEntities())
+            {
+                var tipovi = from c in db.TipTiketa
+                             where c.ponudjena == true
+                             select c;
+
+                foreach (TipTiketa a in tipovi)
+                {
+                    var prviBroj =
+                        from x in db.Tiket
+                        where x.tip.Equals(a.tip) && x.vrijemeDolaska == null
+                        && x.vrijemeIzdavanja.Day == DateTime.Now.Day
+                        && x.vrijemeIzdavanja.Month == DateTime.Now.Month
+                        && x.vrijemeIzdavanja.Year == DateTime.Now.Year
+                        orderby x.vrijemeIzdavanja ascending
+                        select x.redniBroj;
+
+                    if (prviBroj.Any())
+                    {
+                        redovi.Add(a.tip, prviBroj.First());//trenutni broj
+                    }
+                    else
+                    {
+                        redovi.Add(a.tip, 0); ;
+                    }
+                }
+            }
+
+
+            return View(redovi);
         }
 
     }
